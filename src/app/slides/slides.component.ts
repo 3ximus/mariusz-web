907c5839961afc5423b5792e227b2c88f088cf7c
eximus <fabio4335@gmail.com>
2022-01-31 01:50:07 -0500

    add slides

---
+++ src/app/slides/slides.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slides',
+  templateUrl: './slides.component.html',
+  styleUrls: ['./slides.component.scss']
+})
+export class SlidesComponent implements OnInit {
+  imgN = 1;
+  slideSrc = "assets/photos/1.jpg";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeSlide(event: MouseEvent): void {
+    if (event.clientX / window.innerWidth > 0.5) {
+      this.slideSrc = "assets/photos/" + (++this.imgN % 50 + 1)  + ".jpg";
+    } else {
+      this.slideSrc = "assets/photos/" + (--this.imgN % 50 + 1) + ".jpg";
+    }
+  }
+
+}
