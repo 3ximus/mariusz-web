261bf85117413c6ec1f75437888a407301c9a2ff
eximus <fabio4335@gmail.com>
2022-02-04 16:37:46 -0500

    create image elements to load later on

--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -1,25 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+
+const NIMAGES = 50;
 
 @Component({
   selector: 'app-slides',
   templateUrl: './slides.component.html',
   styleUrls: ['./slides.component.scss']
 })
-export class SlidesComponent implements OnInit {
+export class SlidesComponent implements AfterViewInit {
   imgN = 0;
   slideSrc = "assets/photos/1.jpg";
+  images: string[];
+  @ViewChild('imgTarget') imgTarget!: ElementRef;
 
-  constructor() { }
+  constructor() {
+    this.images = Array.from(Array(NIMAGES), (_,i) => "assets/photos/" + (i+1) + ".jpg" );
+  }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const img = new Image();
+    img.src = this.images[0];
+    img.style.height = "inherit";
+    this.imgTarget.nativeElement.appendChild(img);
   }
 
   changeSlide(event: MouseEvent): void {
+
+    if (this.imgTarget.nativeElement.lastChild)
+      this.imgTarget.nativeElement.removeChild(this.imgTarget.nativeElement.lastChild);
+
+    const img = new Image();
     if (event.clientX / window.innerWidth > 0.5) {
-      this.slideSrc = "assets/photos/" + (((++this.imgN % 50) + 50) % 50 + 1)  + ".jpg";
+      img.src = this.images[(((++this.imgN % NIMAGES) + NIMAGES) % NIMAGES)];
+      img.style.height = "inherit";
     } else {
-      this.slideSrc = "assets/photos/" + (((--this.imgN % 50) + 50) % 50 + 1) + ".jpg";
+      const img = new Image();
+      img.src = this.images[(((--this.imgN % NIMAGES) + NIMAGES) % NIMAGES)];
+      img.style.height = "inherit";
     }
+    this.imgTarget.nativeElement.appendChild(img);
   }
 
 }
