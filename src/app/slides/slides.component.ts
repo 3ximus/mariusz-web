32505d03da8c23ddb0ae020c3df0fb9f0ec85ed0
eximus <fabio4335@gmail.com>
2022-02-04 19:55:07 -0500

    image buffer

--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 const NIMAGES = 50;
+const BUFFER_SIZE = 3;
 
 @Component({
   selector: 'app-slides',
@@ -10,35 +11,43 @@ const NIMAGES = 50;
 export class SlidesComponent implements AfterViewInit {
   imgN = 0;
   slideSrc = "assets/photos/1.jpg";
-  images: string[];
+  imageSources: string[];
+  imageBuffer: HTMLImageElement[];
+
   @ViewChild('imgTarget') imgTarget!: ElementRef;
 
   constructor() {
-    this.images = Array.from(Array(NIMAGES), (_,i) => "assets/photos/" + (i+1) + ".jpg" );
+    this.imageSources = Array.from(Array(NIMAGES), (_,i) => "assets/photos/" + (i+1) + ".jpg" );
+    this.imageBuffer = (this.imageSources.slice(-1 - BUFFER_SIZE, -1).concat(this.imageSources.slice(0, 1 + BUFFER_SIZE))).map(src => {
+      const img = new Image();
+      img.src = src;
+      img.style.height = "inherit";
+      return img;
+    });
   }
 
   ngAfterViewInit(): void {
-    const img = new Image();
-    img.src = this.images[0];
-    img.style.height = "inherit";
-    this.imgTarget.nativeElement.appendChild(img);
+    // const img = new Image();
+    // img.src = this.imageSources[0];
+    // img.style.height = "inherit";
+    this.imgTarget.nativeElement.appendChild(this.imageBuffer[BUFFER_SIZE + 0]);
   }
 
   changeSlide(event: MouseEvent): void {
 
-    if (this.imgTarget.nativeElement.lastChild)
-      this.imgTarget.nativeElement.removeChild(this.imgTarget.nativeElement.lastChild);
-
+    // TODO load new image and trouw away old one
     const img = new Image();
     if (event.clientX / window.innerWidth > 0.5) {
-      img.src = this.images[(((++this.imgN % NIMAGES) + NIMAGES) % NIMAGES)];
+      img.src = this.imageSources[(((++this.imgN % NIMAGES) + NIMAGES) % NIMAGES)];
       img.style.height = "inherit";
     } else {
-      const img = new Image();
-      img.src = this.images[(((--this.imgN % NIMAGES) + NIMAGES) % NIMAGES)];
+      img.src = this.imageSources[(((--this.imgN % NIMAGES) + NIMAGES) % NIMAGES)];
       img.style.height = "inherit";
     }
-    this.imgTarget.nativeElement.appendChild(img);
+    img.onload = _ => { // switch images
+      this.imgTarget.nativeElement.appendChild(img);
+      this.imgTarget.nativeElement.removeChild(this.imgTarget.nativeElement.firstChild);
+    }
   }
 
 }
