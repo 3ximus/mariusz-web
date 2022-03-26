2714ce4f65425cc612ebadc76a6affe8c2a7b252
eximus <fabio4335@gmail.com>
2022-03-25 22:39:49 -0400

    weird values for image size

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,6 @@
 export const NIMAGES = 49;
 export const BUFFER_SIZE = 5;
-export const LANDSCAPE_WIDTH = 940;
-export const PORTRAIT_WIDTH = 670;
+export const LANDSCAPE_WIDTH = 1280;
+export const PORTRAIT_WIDTH = 910;
+
 
