5e1cd9e3f8d574dd3f531cf76acff5b2b3608bb9
eximus <fabio4335@gmail.com>
2022-03-26 17:20:15 -0400

    tweaks

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const NIMAGES = 49;
 export const BUFFER_SIZE = 5;
-export const LANDSCAPE_WIDTH = 1280;
-export const PORTRAIT_WIDTH = 910;
+export const LANDSCAPE_WIDTH = 800;
+export const PORTRAIT_WIDTH = 520;
 
 
